1670950f0fcf7519e2eac4327928cce5b55e555f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-09 13:55:39 +0700

    fix error

--- app/src/main/java/id/dutaswalayan/cashier/provider/FeedProvider.java
+++ app/src/main/java/id/dutaswalayan/cashier/provider/FeedProvider.java
@@ -42,22 +42,22 @@ public class FeedProvider extends ContentProvider {
     // When a incoming URI is run through sUriMatcher, it will be tested against the defined
     // URI patterns, and the corresponding route ID will be returned.
     /**
-     * URI ID for route: /entries
+     * URI ID for route: /products
      */
-    public static final int ROUTE_ENTRIES = 1;
+    public static final int ROUTE_PRODUCTS = 1;
 
     /**
-     * URI ID for route: /entries/{ID}
+     * URI ID for route: /products/{ID}
      */
-    public static final int ROUTE_ENTRIES_ID = 2;
+    public static final int ROUTE_PRODUCTS_ID = 2;
 
     /**
      * UriMatcher, used to decode incoming URIs.
      */
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
     static {
-        sUriMatcher.addURI(AUTHORITY, "entries", ROUTE_ENTRIES);
-        sUriMatcher.addURI(AUTHORITY, "entries/*", ROUTE_ENTRIES_ID);
+        sUriMatcher.addURI(AUTHORITY, "products", ROUTE_PRODUCTS);
+        sUriMatcher.addURI(AUTHORITY, "products/*", ROUTE_PRODUCTS_ID);
     }
 
     @Override
@@ -73,10 +73,10 @@ public class FeedProvider extends ContentProvider {
     public String getType(Uri uri) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
-            case ROUTE_ENTRIES:
-                return FeedContract.Entry.CONTENT_TYPE;
-            case ROUTE_ENTRIES_ID:
-                return FeedContract.Entry.CONTENT_ITEM_TYPE;
+            case ROUTE_PRODUCTS:
+                return FeedContract.Product.CONTENT_TYPE;
+            case ROUTE_PRODUCTS_ID:
+                return FeedContract.Product.CONTENT_ITEM_TYPE;
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
@@ -95,13 +95,13 @@ public class FeedProvider extends ContentProvider {
         SelectionBuilder builder = new SelectionBuilder();
         int uriMatch = sUriMatcher.match(uri);
         switch (uriMatch) {
-            case ROUTE_ENTRIES_ID:
+            case ROUTE_PRODUCTS_ID:
                 // Return a single entry, by ID.
                 String id = uri.getLastPathSegment();
-                builder.where(FeedContract.Entry._ID + "=?", id);
-            case ROUTE_ENTRIES:
+                builder.where(FeedContract.Product._ID + "=?", id);
+            case ROUTE_PRODUCTS:
                 // Return all known entries.
-                builder.table(FeedContract.Entry.TABLE_NAME)
+                builder.table(FeedContract.Product.TABLE_NAME)
                         .where(selection, selectionArgs);
                 Cursor c = builder.query(db, projection, sortOrder);
                 // Note: Notification URI must be manually set here for loaders to correctly
@@ -125,11 +125,11 @@ public class FeedProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
         Uri result;
         switch (match) {
-            case ROUTE_ENTRIES:
-                long id = db.insertOrThrow(FeedContract.Entry.TABLE_NAME, null, values);
-                result = Uri.parse(FeedContract.Entry.CONTENT_URI + "/" + id);
+            case ROUTE_PRODUCTS:
+                long id = db.insertOrThrow(FeedContract.Product.TABLE_NAME, null, values);
+                result = Uri.parse(FeedContract.Product.CONTENT_URI + "/" + id);
                 break;
-            case ROUTE_ENTRIES_ID:
+            case ROUTE_PRODUCTS_ID:
                 throw new UnsupportedOperationException("Insert not supported on URI: " + uri);
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
@@ -151,15 +151,15 @@ public class FeedProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
         int count;
         switch (match) {
-            case ROUTE_ENTRIES:
-                count = builder.table(FeedContract.Entry.TABLE_NAME)
+            case ROUTE_PRODUCTS:
+                count = builder.table(FeedContract.Product.TABLE_NAME)
                         .where(selection, selectionArgs)
                         .delete(db);
                 break;
-            case ROUTE_ENTRIES_ID:
+            case ROUTE_PRODUCTS_ID:
                 String id = uri.getLastPathSegment();
-                count = builder.table(FeedContract.Entry.TABLE_NAME)
-                        .where(FeedContract.Entry._ID + "=?", id)
+                count = builder.table(FeedContract.Product.TABLE_NAME)
+                        .where(FeedContract.Product._ID + "=?", id)
                         .where(selection, selectionArgs)
                         .delete(db);
                 break;
@@ -183,15 +183,15 @@ public class FeedProvider extends ContentProvider {
         final int match = sUriMatcher.match(uri);
         int count;
         switch (match) {
-            case ROUTE_ENTRIES:
-                count = builder.table(FeedContract.Entry.TABLE_NAME)
+            case ROUTE_PRODUCTS:
+                count = builder.table(FeedContract.Product.TABLE_NAME)
                         .where(selection, selectionArgs)
                         .update(db, values);
                 break;
-            case ROUTE_ENTRIES_ID:
+            case ROUTE_PRODUCTS_ID:
                 String id = uri.getLastPathSegment();
-                count = builder.table(FeedContract.Entry.TABLE_NAME)
-                        .where(FeedContract.Entry._ID + "=?", id)
+                count = builder.table(FeedContract.Product.TABLE_NAME)
+                        .where(FeedContract.Product._ID + "=?", id)
                         .where(selection, selectionArgs)
                         .update(db, values);
                 break;
@@ -214,23 +214,23 @@ public class FeedProvider extends ContentProvider {
         /** Schema version. */
         public static final int DATABASE_VERSION = 1;
         /** Filename for SQLite file. */
-        public static final String DATABASE_NAME = "feed.db";
+        public static final String DATABASE_NAME = "product.db";
 
         private static final String TYPE_TEXT = " TEXT";
         private static final String TYPE_INTEGER = " INTEGER";
         private static final String COMMA_SEP = ",";
         /** SQL statement to create "entry" table. */
         private static final String SQL_CREATE_ENTRIES =
-                "CREATE TABLE " + FeedContract.Entry.TABLE_NAME + " (" +
-                        FeedContract.Entry._ID + " INTEGER PRIMARY KEY," +
-                        FeedContract.Entry.COLUMN_NAME_ENTRY_ID + TYPE_TEXT + COMMA_SEP +
-                        FeedContract.Entry.COLUMN_NAME_TITLE    + TYPE_TEXT + COMMA_SEP +
-                        FeedContract.Entry.COLUMN_NAME_LINK + TYPE_TEXT + COMMA_SEP +
-                        FeedContract.Entry.COLUMN_NAME_PUBLISHED + TYPE_INTEGER + ")";
+                "CREATE TABLE " + FeedContract.Product.TABLE_NAME + " (" +
+                        FeedContract.Product._ID + " INTEGER PRIMARY KEY," +
+                        FeedContract.Product.COLUMN_PRODUCT_ID + TYPE_TEXT + COMMA_SEP +
+                        FeedContract.Product.COLUMN_DESCRIPTION    + TYPE_TEXT + COMMA_SEP +
+                        FeedContract.Product.COLUMN_UNIT + TYPE_TEXT + COMMA_SEP +
+                        FeedContract.Product.COLUMN_PRICE + TYPE_INTEGER + ")";
 
         /** SQL statement to drop "entry" table. */
         private static final String SQL_DELETE_ENTRIES =
-                "DROP TABLE IF EXISTS " + FeedContract.Entry.TABLE_NAME;
+                "DROP TABLE IF EXISTS " + FeedContract.Product.TABLE_NAME;
 
         public FeedDatabase(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
