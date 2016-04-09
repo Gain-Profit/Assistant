97891673cbec48a07f187a5c57de98ef065e1d9a
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-09 13:47:12 +0700

    Feed Contract

---
+++ app/src/main/java/id/dutaswalayan/cashier/provider/FeedContract.java
@@ -0,0 +1,69 @@
+package id.dutaswalayan.cashier.provider;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class FeedContract {
+    private FeedContract(){
+    }
+
+
+    /**
+     * Content provider authority.
+     */
+    public static final String CONTENT_AUTHORITY = "id.dutaswalayan.cashier";
+
+    /**
+     * Base URI. (content://id.dutaswalayan.cashier)
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Path component for "product"-type resources..
+     */
+    private static final String PATH_PRODUCTS = "products";
+
+    /**
+     * Columns supported by "products" records.
+     */
+    public static class Product implements BaseColumns {
+        /**
+         * MIME type for lists of entries.
+         */
+        public static final String CONTENT_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/vnd.cashier.products";
+        /**
+         * MIME type for individual entries.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/vnd.cashier.product";
+
+        /**
+         * Fully qualified URI for "entry" resources.
+         */
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_PRODUCTS).build();
+
+        /**
+         * Table name where records are stored for "product" resources.
+         */
+        public static final String TABLE_NAME = "product";
+        /**
+         * Atom ID. (Note: Not to be confused with the database primary key, which is _ID.
+         */
+        public static final String COLUMN_PRODUCT_ID = "product_id";
+        /**
+         * Product Description
+         */
+        public static final String COLUMN_DESCRIPTION = "description";
+        /**
+         * Unit for Product "PIECES, BOX, etc."
+         */
+        public static final String COLUMN_UNIT = "unit";
+        /**
+         * Price for Product.
+         */
+        public static final String COLUMN_PRICE = "price";
+    }
+}
