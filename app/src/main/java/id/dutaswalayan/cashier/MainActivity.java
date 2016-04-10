3e257e162cf56420a0de5376db5e73197ff7b60d
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-10 22:35:44 +0700

    filter data on list view.

--- app/src/main/java/id/dutaswalayan/cashier/MainActivity.java
+++ app/src/main/java/id/dutaswalayan/cashier/MainActivity.java
@@ -1,6 +1,9 @@
 package id.dutaswalayan.cashier;
 
 import android.app.LoaderManager;
+import android.app.SearchManager;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -8,6 +11,9 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,6 +24,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.database.Cursor;
+import android.widget.FilterQueryProvider;
 import android.widget.ListView;
 
 import id.dutaswalayan.cashier.provider.FeedContract;
@@ -29,6 +36,7 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
 
     private ListView mListView;
+    private SearchView searchView;
 
     /**
      * Cursor adapter for controlling ListView results.
@@ -121,6 +129,20 @@ public class MainActivity extends AppCompatActivity
         );
 
         mListView.setAdapter(mAdapter);
+        mListView.setTextFilterEnabled(true);
+
+        mAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            @Override
+            public Cursor runQuery(CharSequence constraint) {
+                final ContentResolver contentResolver = getContentResolver();
+                Cursor c = contentResolver.query(FeedContract.Product.CONTENT_URI,
+                        PROJECTION,
+                        FeedContract.Product.COLUMN_DESCRIPTION + " LIKE ?",
+                        new String[]{"%" + constraint.toString() + "%"},
+                        FeedContract.Product.COLUMN_DESCRIPTION + " asc"); // Sort
+                return c;
+            }
+        });
         getLoaderManager().initLoader(0, null, this);
     }
 
@@ -138,6 +160,22 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+
         return true;
     }
 
