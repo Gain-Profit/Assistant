b951aa99aa454c006618790c668e511a9c00b58f
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-16 11:29:08 +0700

    URL products.

--- app/src/main/java/id/dutaswalayan/cashier/sync/SyncAdapter.java
+++ app/src/main/java/id/dutaswalayan/cashier/sync/SyncAdapter.java
@@ -65,7 +65,7 @@ class SyncAdapter extends AbstractThreadedSyncAdapter {
      * <p>This points to the Android Developers Blog. (Side note: We highly recommend reading the
      * Android Developer Blog to stay up to date on the latest Android platform developments!)
      */
-    private static final String FEED_URL = "http://10.0.3.2/dutaswalayan/products.json";
+    private static final String FEED_URL = "http://api.dutaswalayan.com/products";
 
     /**
      * Network connection timeout, in milliseconds.
