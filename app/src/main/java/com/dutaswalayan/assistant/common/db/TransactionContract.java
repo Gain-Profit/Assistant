563680152e293f9a97392fc93a2d6eab7c727914
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-24 19:56:32 +0700

    table transaction
    column from product plus qty

--- app/src/main/java/com/dutaswalayan/assistant/common/db/TransactionContract.java
+++ app/src/main/java/com/dutaswalayan/assistant/common/db/TransactionContract.java
@@ -6,6 +6,12 @@ public class TransactionContract {
     private TransactionContract(){}
 
     public static class Transaction implements BaseColumns {
-        
+        public static final String TABLE_NAME = "transaction";
+        public static final String COLUMN_PRODUCT_ID = "product_id";
+        public static final String COLUMN_DESCRIPTION = "description";
+        public static final String COLUMN_UNIT = "unit";
+        public static final String COLUMN_PRICE = "price";
+        public static final String COLUMN_BARCODE = "barcode";
+        public static final String COLUMN_QTY = "qty";
     }
 }
