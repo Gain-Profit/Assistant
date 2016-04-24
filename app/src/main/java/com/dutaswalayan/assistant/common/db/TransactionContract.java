f4cd2a785a0475f5bc007135d17103dcb02e0adc
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-24 20:10:13 +0700

    TransactionDatabase.

--- app/src/main/java/com/dutaswalayan/assistant/common/db/TransactionContract.java
+++ app/src/main/java/com/dutaswalayan/assistant/common/db/TransactionContract.java
@@ -1,5 +1,8 @@
 package com.dutaswalayan.assistant.common.db;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.provider.BaseColumns;
 
 public class TransactionContract {
@@ -13,5 +16,45 @@ public class TransactionContract {
         public static final String COLUMN_PRICE = "price";
         public static final String COLUMN_BARCODE = "barcode";
         public static final String COLUMN_QTY = "qty";
+    }
+
+    public static class TransactionDatabase extends SQLiteOpenHelper {
+        /** Schema version. */
+        public static final int DATABASE_VERSION = 1;
+        /** Filename for SQLite file. */
+        public static final String DATABASE_NAME = "transaction.db";
+
+        private static final String TYPE_TEXT = " TEXT";
+        private static final String TYPE_INTEGER = " INTEGER";
+        private static final String COMMA_SEP = ",";
+        /** SQL statement to create "transaction" table. */
+        private static final String SQL_CREATE_TRANSACTION =
+                "CREATE TABLE " + Transaction.TABLE_NAME + " (" +
+                        Transaction._ID + " INTEGER PRIMARY KEY," +
+                        Transaction.COLUMN_PRODUCT_ID + TYPE_TEXT + COMMA_SEP +
+                        Transaction.COLUMN_DESCRIPTION    + TYPE_TEXT + COMMA_SEP +
+                        Transaction.COLUMN_UNIT + TYPE_TEXT + COMMA_SEP +
+                        Transaction.COLUMN_PRICE + TYPE_INTEGER + COMMA_SEP +
+                        Transaction.COLUMN_BARCODE + TYPE_TEXT + COMMA_SEP +
+                        Transaction.COLUMN_QTY + TYPE_INTEGER + ")";
+
+        /** SQL statement to drop "entry" table. */
+        private static final String SQL_DELETE_TRANSACTION =
+                "DROP TABLE IF EXISTS " + Transaction.TABLE_NAME;
+
+        public TransactionDatabase(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(SQL_CREATE_TRANSACTION);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(SQL_DELETE_TRANSACTION);
+            onCreate(db);
+        }
     }
 }
