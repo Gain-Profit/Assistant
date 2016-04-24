bdbba35958b5d7824d6b875cf7c430df04016602
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-24 21:00:35 +0700

    SQLiteDatabase.

--- app/src/main/java/com/dutaswalayan/assistant/TransactionActivity.java
+++ app/src/main/java/com/dutaswalayan/assistant/TransactionActivity.java
@@ -1,14 +1,20 @@
 package com.dutaswalayan.assistant;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import com.dutaswalayan.assistant.common.db.TransactionContract;
 
 public class TransactionActivity extends AppCompatActivity {
-
+    TransactionContract.TransactionDatabase mDbTransaction;
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +31,15 @@ public class TransactionActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mDbTransaction = new TransactionContract.TransactionDatabase(this);
+        list = (ListView) findViewById(R.id.listTransaction);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SQLiteDatabase db = mDbTransaction.getWritableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM transaction",null);
+    }
 }
