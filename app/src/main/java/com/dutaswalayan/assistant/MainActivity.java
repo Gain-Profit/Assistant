40db66d8279950cb6bcf4843536cd39f9c8cc4b4
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-04-25 17:18:05 +0700

    delete setting menu from main activity

--- app/src/main/java/com/dutaswalayan/assistant/MainActivity.java
+++ app/src/main/java/com/dutaswalayan/assistant/MainActivity.java
@@ -253,11 +253,6 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == R.id.menu_refresh) {
             SyncUtils.TriggerRefresh();
         }
