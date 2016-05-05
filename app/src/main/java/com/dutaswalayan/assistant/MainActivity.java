9f7d90f91de2f3806f725bd8a0f0e8a9e4f64ad6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2016-05-05 18:03:51 +0700

    email

--- app/src/main/java/com/dutaswalayan/assistant/MainActivity.java
+++ app/src/main/java/com/dutaswalayan/assistant/MainActivity.java
@@ -271,6 +271,7 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
+            //send email...
 
         }
 
